f73480f46ae842f0786f1ce0cc2fcea9dddafd82
Yeonwoo Kim <simone0109241@gmail.com>
2022-01-02 18:09:33 -0600

    Update 4_keyboard_event.py
    x, y position

--- pygame_basic/4_keyboard_event.py
+++ pygame_basic/4_keyboard_event.py
@@ -51,6 +51,17 @@ while running:
                 to_y = 0
 character_x_pos += to_x
 character_y_pos += to_y
+# 가로 경계값 처리
+if character_x_pos < 0:
+    character_x_pos = 0
+elif character_x_pos > screen_width - character_width:
+    character_x_pos = screen_width-character_width
+# 세로 경계값 처리
+if character_y_pos < 0:
+    character_y_pos = 0
+elif character_y_pos > screen_width - character_height:
+    character_y_pos = screen_width-character_height
+
 
 screen.blit(background, (0, 0))  # 배경그리기
 
